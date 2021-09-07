e236a4190c3de39f0526dd8a14548d67aab1399f
snehal <thoratsnehal17@gmail.com>
2021-09-07 17:39:21 +0530

    UC1 added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,8 +1,17 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static void main(String[] args)
+    public  static void main(String args[])
     {
-        System.out.println("welcome");
+        int IS_FULL_TIME=1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME)
+        {
+            System.out.println("Employee is present.");
+        }
+        else
+        {
+            System.out.println("Employee is absent.");
+        }
     }
 }
