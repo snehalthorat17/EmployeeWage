c81ca62970278ab6b56f47c2e85f8b0bc67c40f4
snehal <thoratsnehal17@gmail.com>
2021-09-07 18:00:05 +0530

    UC4 added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,23 +1,26 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    static int EMP_RATE_PER_HOUR = 20;
+
     public  static void main(String args[])
     {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == IS_FULL_TIME)
-        {
-            empHrs = 12;
-        }
-        else if (empCheck == IS_PART_TIME)
-        {
-            empHrs = 8;
+        int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+
+        switch (empCheck){
+            case IS_FULL_TIME:
+                empHrs = 12;
+                break;
+            case IS_PART_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("EMP wage is: "+empWage);
